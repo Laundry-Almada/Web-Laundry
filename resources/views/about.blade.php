4c5e1eae8bc87a0f3b82976bbee6a49323bb8161
abiazfazelms <abiazfazel.solo@gmail.com>
2025-05-11 18:29:25 +0700

    edit lorem about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,171 +5,111 @@
     <link href="https://fonts.googleapis.com/css?family=Cabin&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Knewave&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Actor&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Jockey+One&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
-    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Almada Laundry</title>
+    <link href="{{ asset('css/about.css') }}" rel="stylesheet">
+    <title>About Us</title>
 </head>
 
 <body>
-    {{-- <div class="container"> --}}
-    <div class="v292_610">
-        <div class="content-wrapper">
-            <div class="v292_611"><span class="v292_612">WELCOME!</span><span class="v292_613">Selamat datang di Almada Laundry! 
-            Kami menyediakan layanan laundry 
-            berkualitas dengan harga yang terjangkau 
-            dan pelayanan yang ramah. 
-                </span>
-                <div class="v292_614"></div><span class="v292_615">Cek Laundry Kamu</span>
-                <div class="v292_616"></div>
-            </div>
-            <div class="v292_617">
-                <div class="v292_618"></div><span class="v292_619">ALMADA LAUNDRY</span>
-                <div class="v292_620"></div><a href="{{ route('home') }}"><span class="v292_621">HOME</span></a>
-                <div class="v292_622"></div>
-                <div class="v292_623"></div>
-                <div class="v292_624"></div>
-                <div class="v292_625"></div><a href="{{ route('about') }}"><span class="v292_626">ABOUT US</span></a><a
-                    href="{{ route('services') }}"><span class="v292_627">SERVICES</span></a><a
-                    href="{{ route('pricing') }}"><span class="v292_628">PRICING</span></a><span
-                    class="v292_629">CONTACT</span>
-            </div>
-            <div class="v292_630">
-                <div class="v292_631">
-                    <div class="v292_632"></div>
-                </div>
-                <div class="v292_633">
-                    <div class="v292_634"></div>
-                </div>
-                <div class="v292_635">
-                    <div class="v292_636"></div>
-                </div>
-                <div class="v292_637"></div><span class="v292_638">WHY CHOOSE US ?</span>
-                <div class="v292_640"></div>
-            </div>
-            <div class="v349_121">
-                <div class="v349_122">
-                    <div class="v349_123"></div>
-                </div>
-                <div class="v349_124">
-                    <div class="v349_125"></div>
-                </div>
-                <div class="v349_126">
-                    <div class="v349_127"></div>
-                </div>
-                <div class="v349_128"></div><span class="v349_129">WHY CHOOSE US ?</span>
-                <div class="v349_131"></div><span class="v349_132">Kami memberikan pelayanan
-                    lebih personal dengan 
-                    hubungan yang akrab bersama 
-                    pelanggan. Kamu bisa 
-                    mengecek status cucian kapan 
-                    saja, bahkan di luar jam kerja.
-                </span><span class="v349_133">Personalized Service</span><span class="v349_134">Affordable and Competitive Pricing</span><span
-                    class="v349_135">Fast Service</span><span class="v349_136">Customer Trust</span><span
-                    class="v349_137">Harga yang terjangkau untuk berbagai layanan laundry, mulai dari cuci baju hingga dry cleaning.
-                </span><span class="v349_138">Layanan ekspres yang memungkinkan cucian kamu selesai dalam 1 hari.
-                </span><span class="v349_139">Pelanggan kami selalu puas dengan keramahan dan kecepatan layanan kami, termasuk pelanggan dari Grand Amira Hotel dan Hotel NEO Gading Solo.
-                </span>
-            </div>
-            <div class="v292_649">
-                <div class="v292_650"></div><span class="v292_651">Phone +62 812345678</span><span
-                    class="v292_652">Email
-                    almadalaundry@gmail.com</span>
-                <div class="v292_653"></div>
-                <div class="v292_659">
+    <div class="v292_472">
+        <div class="v292_473">
+            <div class="v292_749"></div>
+            <div class="v292_751"></div><span class="v292_475">Home / About Us</span><span class="v292_474">ABOUT
+                US</span>
+        </div>
+        <div class="v292_479">
+            <div class="v292_480"></div><span class="v292_481">ALMADA LAUNDRY</span>
+            <div class="v292_482"></div>
+            <a href="{{ route('home') }}"><span class="v292_483">HOME</span></a>
+            <div class="v292_484"></div>
+            <div class="v292_485"></div>
+            <div class="v292_486"></div>
+            <div class="v292_487"></div>
+            <a href="{{ route('about') }}"><span class="v292_488">ABOUT US</span></a>
+            <a href="{{ route('services') }}"><span class="v292_489">SERVICES</span></a>
+            <a href="{{ route('pricing') }}"><span class="v292_490">PRICING</span></a>
+            <a href="#contact"><span class="v292_491">CONTACT</span></a>
+        </div>
+        <div class="v292_545">
+            <div class="v292_546"><span class="v292_547">Get In Touch</span>
+                <div class="name"></div>
+                <div class="v292_549"></div>
+                <div class="v292_550">
                     <div class="name"></div>
+                </div><span class="v292_552">Phone
+                    +62 812345678</span>
+                <div class="v292_553"></div><span class="v292_554">Email
+                    almadalaundry@gmail.com</span><span class="v292_555">Website
+                    www.almadalaundry.com</span><span class="v292_556">Address
+                    Jl. Sampangan No.92, Semanggi,
+                    Kec. Ps. Kliwon, Kota Surakarta,
+                    Jawa Tengah 57191</span>
+                <div class="name"></div>
+            </div>
+            <div class="v292_558"><span class="v292_559">ALMADA LAUNDRY</span><span class="v292_560">Almada Laundry adalah penyedia layanan laundry terpercaya dengan pelayanan cepat, ramah, dan kualitas terbaik. Kami berkomitmen untuk memberikan kenyamanan kepada setiap pelanggan, dengan berbagai layanan yang dapat disesuaikan dengan kebutuhan Anda.
+                </span>
+                <div class="name"></div>
+                <div class="v292_562">
+                    <div class="v292_563"></div>
+                    <div class="v292_564"></div>
                 </div>
-                <a href="{{ route('login') }}">
-                    <div class="v349_294">
-                        <div class="v349_293"></div><span class="v349_295">LOGIN</span>
-                    </div>
-                </a>
-                <a href="{{ route('register') }}">
-                    <div class="v349_296">
-                        <div class="v349_297"></div><span class="v349_298">REGISTER</span>
-                    </div>
-                </a>
+                <div class="name"></div>
+            </div>
+        </div>
+        <div class="v347_139"><span class="v347_140">WELCOME TO ALMADA LAUNDRY</span><span class="v347_141">Almada Laundry hadir untuk memberikan solusi laundry yang cepat, terpercaya, dan berkualitas.
+            </span></div>
+        <div class="v349_111"></div>
+        <div class="v349_113"><span class="v349_114">OUR HISTORY</span><span class="v349_115">Almada Laundry dimulai dari sebuah ide sederhana yang muncul setelah melihat adanya peluang besar dalam bisnis laundry di tengah kurangnya persaingan pada saat itu. Pemilik usaha, yang sebelumnya menjalankan usaha pakaian, berkenalan dengan seorang teman yang telah lebih dulu menggeluti bisnis laundry. Melalui diskusi yang panjang, teman tersebut menjelaskan bahwa bisnis laundry dapat dijalankan dengan modal yang relatif kecil dibandingkan dengan bisnis pakaian yang membutuhkan biaya besar untuk stok barang.
 
+Bermodal mesin cuci dan setrika, serta tekad untuk memberikan layanan terbaik, Almada Laundry mulai beroperasi. Pada awalnya, usaha ini hanya melayani kebutuhan laundry pribadi dan rumah tangga sekitar, namun seiring berjalannya waktu, pelanggan mulai berdatangan dan bisnis pun berkembang. Terlihat adanya potensi yang besar untuk mengembangkan usaha laundry ini lebih jauh, terutama dengan adanya layanan yang lebih personal dan lebih cepat dibandingkan kompetitor saat itu.
+
+Dengan visi yang jelas untuk memberikan kenyamanan kepada pelanggan dan membangun hubungan yang lebih akrab, Almada Laundry terus berkembang hingga saat ini, menjadi pilihan utama bagi banyak pelanggan, termasuk hotel-hotel ternama seperti Horison Aziza Solo, Grand Amira Hotel, dan Hotel NEO Gading Solo.</span></div>
+        <div class="v349_146"><span class="v349_147">DRY CLEANING</span><span class="v349_148">Dry cleaning adalah layanan pembersihan pakaian menggunakan proses kimia tanpa menggunakan air. Metode ini sangat cocok untuk pakaian dengan bahan halus atau berwarna gelap, seperti gaun pesta, jas, dan pakaian berbahan sutra atau wol, yang membutuhkan perawatan khusus agar tidak rusak. Dengan dry cleaning, pakaian tetap terjaga kualitasnya, bebas dari noda, dan kembali dalam kondisi sempurna tanpa khawatir kehilangan bentuk atau warna.</span>
+            <div class="v349_142"></div>
+        </div>
+        <div class="v349_158"><span class="v349_159">SETRIKA KILAT</span><span class="v349_160">Setrika Kilat adalah layanan penyetrikaan pakaian yang dilakukan dengan cepat namun tetap menjaga hasil yang maksimal. Jika Anda membutuhkan pakaian yang sudah rapi dan siap dipakai dalam waktu singkat, layanan ini adalah solusi terbaik. Kami menggunakan alat setrika profesional yang dapat menghasilkan hasil yang rapi tanpa merusak bahan pakaian, sehingga Anda tetap tampil percaya diri dengan pakaian yang terjaga kualitasnya.</span>
+            <div class="v349_164"></div>
+        </div>
+        <div class="v349_150"><span class="v349_151">SPECIAL ITEMS</span><span class="v349_152">Natur adalah layanan khusus yang bertujuan untuk mengembalikan warna pakaian yang telah pudar atau kusam akibat pemakaian atau pencucian berkali-kali. Dengan menggunakan bahan alami dan teknik tertentu, layanan ini membantu memperbarui warna pakaian, sehingga pakaian terlihat lebih segar, cerah, dan seolah baru kembali. Ini sangat bermanfaat untuk pakaian berwarna cerah atau pakaian yang memiliki warna yang mulai memudar setelah beberapa kali pencucian.</span>
+            <div class="v349_156"></div>
+        </div>
+        <div class="v349_166"><span class="v349_167">CUCI KILAT</span><span class="v349_168">Cuci Kilat adalah layanan laundry ekspres yang dirancang untuk pelanggan yang membutuhkan pakaian dicuci dan disetrika dalam waktu yang sangat singkat. Layanan ini cocok untuk Anda yang memiliki jadwal padat dan membutuhkan pakaian yang siap pakai dalam waktu cepat. Kami menjamin bahwa pakaian Anda akan tetap bersih dan rapi meskipun prosesnya dilakukan secara kilat.</span>
+            <div class="v349_172"></div>
+        </div>
+        <div class="v349_174">
+            <div class="v349_145"></div>
+            <div class="v349_264"><span class="v349_265">OUR VISION</span><span class="v349_266">Almada Laundry mengembangkan usaha laundry ini menjadi lebih besar di masa depan dengan menciptakan kenyamanan bagi pelanggan melalui pembentukan hubungan yang akrab dan peningkatan kepercayaan, yang menjadi kekuatan utama dalam menarik pelanggan untuk kembali.</span>
+                <div class="v349_279"></div>
+                <div class="name"></div>
             </div>
-            <div class="v292_661"><span class="v292_662">OUR SERVICE</span><span class="v292_663">Layanan Almada Laundry
-                </span><span class="v292_664">Cuci Baju Reguler</span>
-                <div class="v292_666"></div>
-                <div class="v292_667"></div><span class="v292_668">Cuci Ekspres
-                </span><span
-                    class="v292_669">Dry Cleaning</span>
-                <div class="v292_672"></div>
-                <div class="v292_673"></div><span class="v292_674">Setrika Ekspres</span><span
-                    class="v292_675">Cuci Karpet, Cuci Gordain, Cuci Sepatu, Cuci Boneka</span><span class="v292_676">Natur (Mengembalikan Warna Pakaian)</span>
-                <div class="v292_680"></div>
-                <div class="v292_681"></div>
-                <div class="v292_682"></div>
+            <div class="v349_268"><span class="v349_269">OUR MISSION</span><span class="v349_270">Almada Laundry meningkatkan fasilitas, seperti menambah mesin pengering, agar usaha tetap dapat beroperasi meskipun cuaca hujan, serta mengembangkan usaha ini lebih besar dalam jangka panjang.</span>
+                <div class="v349_285"></div>
+                <div class="name"></div>
             </div>
-            <div class="v292_683">
-                <div class="v292_684"><span class="v292_685">Get In Touch</span>
-                    <div class="name"></div>
-                    <div class="v292_687"></div>
-                    <div class="v292_688">
-                        <div class="name"></div>
-                    </div><span class="v292_690">Phone
-                        +62 812345678</span>
-                    <div class="v292_691"></div><span class="v292_692">Email
-                        almadalaundry@gmail.com</span><span class="v292_693">Website
-                        www.almadalaundry.com</span><span class="v292_694">Address
-                        Jl. Sampangan No.92, Semanggi,
-                        Kec. Ps. Kliwon, Kota Surakarta,
-                        Jawa Tengah 57191</span>
-                    <div class="name"></div>
-                </div>
-                <div class="v292_696"><span class="v292_697">ALMADA LAUNDRY</span><span class="v292_698">Almada Laundry adalah penyedia layanan 
-                    laundry terpercaya dengan pelayanan cepat, 
-                    ramah, dan kualitas terbaik. Kami berkomitmen 
-                    untuk memberikan kenyamanan kepada setiap 
-                    pelanggan, dengan berbagai layanan yang 
-                    dapat disesuaikan dengan kebutuhan Anda.
-                    </span>
-                    <div class="name"></div>
-                    <div class="v292_700">
-                        <div class="v292_701"></div>
-                        <div class="v292_702"></div>
-                    </div>
-                    <div class="name"></div>
-                </div>
+            <div class="v349_272"><span class="v349_273">OUR STRENGTHS</span><span class="v349_274">Almada Laundry menawarkan pelayanan personal dengan hubungan akrab, memungkinkan pelanggan untuk mengecek status cucian melalui WhatsApp kapan saja, bahkan di luar jam kerja, demi kenyamanan dan kepuasan mereka.</span>
+                <div class="v349_280"></div>
             </div>
-            <div class="v292_704">
-                <div class="v292_705"></div><span class="v292_706">PICK-UP AND DELIVERY ORDERS</span><span
-                    class="v292_707">250+</span><span class="v292_708">2000+</span><span
-                    class="v292_709">1000+</span><span class="v292_710">500+</span><span
-                    class="v292_711">LAUNDRY LOADS PROCESSED</span><span class="v292_712">CUSTOMERS</span><span
-                    class="v292_713">POSITIVE REVIEWS</span>
-                <div class="v292_714"></div>
-                <div class="v292_715"></div>
-                <div class="v292_716"></div>
-                <div class="v292_717"></div>
+            <div class="name"></div>
+        </div>
+        <div class="v349_309">
+            <div class="v349_310"></div><span class="v349_311">Phone +62 812345678</span><span class="v349_312">Email
+                almadalaundry@gmail.com</span>
+            <div class="v349_313"></div>
+            <div class="v349_314">
+                <div class="name"></div>
             </div>
-            <div class="v292_718"><span class="v292_719">TESTIMONIALS</span>
-                <div class="v292_721"></div>
-                <div class="v292_722"></div>
-                <div class="v292_723"></div>
-                <div class="v292_724"></div><span class="v292_725">39Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.
-                </span><span class="v292_726">40Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.
-                </span><span class="v292_727">41Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.
-                </span><span class="v292_728">42Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.
-                </span><span class="v292_729">wwwwlinkwww</span><span class="v292_730">wwwwlinkwww</span><span
-                    class="v292_731">wwwwlinkwww</span><span class="v292_732">wwwwlinkwww</span><span
-                    class="v292_733">NAME</span><span class="v292_734">NAME</span><span
-                    class="v292_735">NAME</span><span class="v292_736">NAME</span>
+            <div class="v349_316">
+                <div class="v349_317"></div>
+                <a href="{{ route('login') }}"><span class="v349_318">LOGIN</span></a>
+            </div>
+            <div class="v349_319">
+                <div class="v349_320"></div>
+                <a href="{{ route('register') }}"><span class="v349_321">REGISTER</span></a>
             </div>
         </div>
     </div>
-    {{-- </div> --}}
 </body>
 
-
+</html>
